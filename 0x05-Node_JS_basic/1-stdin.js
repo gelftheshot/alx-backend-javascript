865b9c45ec60d06685dba4323b883b927265c042
Leon Gebre <gelf.learing@gmail.com>
2024-06-03 19:57:55 +0300

    fixing one way

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,8 @@ process.stdin.on('readable', () => {
   const readed = process.stdin.read();
   if (readed !== null) {
     process.stdout.write(`Your name is: ${readed}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
   }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
