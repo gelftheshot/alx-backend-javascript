c597ffa871bf82ea8edadd7ab50b9e30f851ab1d
Leon Gebre <gelf.learing@gmail.com>
2024-06-03 17:42:15 +0300

    cling esling mess

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,9 +4,9 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
 
-    const lines = data.split('\n').filter(line => line);
-    const students = lines.slice(1).map(line => line.split(','));
-
+    const lines = data.split('\n').filter((line) => line);
+    const students = lines.slice(1).map((line) => line.split(','));
+    console.log(students);
     console.log(`Number of students: ${students.length}`);
 
     const fields = {};
@@ -19,7 +19,9 @@ function countStudents(path) {
     }
 
     for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
