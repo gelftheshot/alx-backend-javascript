80f1e583c86cd1d247c55eefa78c8acccdcdd916
Leon Gebre <gelf.learing@gmail.com>
2024-06-04 20:06:57 +0300

    changin the variable name to app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
 const port = 1245;
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log('...');
 });
