64e3e8284549b61a1e527aeefbaa5ae0932190aa
Leon Gebre <gelf.learing@gmail.com>
2024-06-04 22:16:51 +0300

    reading the database and returing the array of the first name for every field avalable out htere

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+export default function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      const rows = data.trim().split('\n');
+
+      const fieldObj = {};
+
+      for (let i = 1; i < rows.length; i += 1) {
+        const row = rows[i].split(',');
+        const firstName = row[0];
+        const field = row[3];
+
+        if (!fieldObj[field]) {
+          fieldObj[field] = [];
+        }
+        fieldObj[field].push(firstName);
+      }
+
+      resolve(fieldObj);
+    });
+  });
+}
