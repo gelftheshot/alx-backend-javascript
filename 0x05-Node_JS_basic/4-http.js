82fc11bdea4f3c160074252c5953db8c7f8704a4
Leon Gebre <gelf.learing@gmail.com>
2024-06-04 20:14:25 +0300

    adding header and status of the code

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
 const port = 1245;
 
-app.listen(port, () => {
-  console.log('...');
+app.listen(port, '127.0.0.1', () => {
 });
 
-export default app;
+module.exports = app;
