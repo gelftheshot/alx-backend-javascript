9adeeb9f0b18957cd7274625b12d2c19afc03711
Leon Gebre <gelf.learing@gmail.com>
2024-06-04 12:21:03 +0300

    updating the way of promis returned

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,11 @@
 const signUpUser = require('./4-user-promise').default;
 const uploadPhoto = require('./5-photo-reject').default;
 
-function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    }
+    )));
 }
-
-module.exports = handleProfileSignup;
