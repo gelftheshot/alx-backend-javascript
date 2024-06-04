f17050e6b7feb29dd5c8ee664f77aee766a0bc5f
Leon Gebre <gelf.learing@gmail.com>
2024-06-04 20:34:02 +0300

    return 404 if the code doesn't exit

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,20 +3,21 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+  } else if (req.url === '/students') {
     countStudents(process.argv[2])
       .then((data) => {
-        res.write('');
         res.end(`This is the list of our students\n${data.join('\n')}`);
       })
       .catch((error) => {
         res.end(error.message);
       });
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
 });
 
