1af93819fd5c1f3d9dbeeb629c1a9205a4e55b41
Leon Gebre <gelf.learing@gmail.com>
2024-06-07 23:15:07 +0300

    using before each and after each

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const mocha = require('mocha');
+const { expect, assert } = require('chai');
+const sinon = require('sinon');
+
+const utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" for inputs 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.equal(true);
+  });
+
+  it('should log "The total is: 20" for inputs 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.equal(true);
+  });
+});
